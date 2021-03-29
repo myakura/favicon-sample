69c5f5ea0d817b9086c9ba61c459b28fa200f808
Masataka Yakura <masataka.yakura@gmail.com>
2021-03-30 07:36:12 +0900

    typeMismatch: add data and template

---
+++ source/_data/typeMismatch.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    name: 'type-ico-file-png',
+    type: 'image/vnd.microsoft.icon',
+    path: '/assets/icons/png/icon-16.png',
+  },
+  {
+    name: 'type-jpeg-file-png',
+    type: 'image/jpeg',
+    path: '/assets/icons/png/icon-16.png',
+  },
+  {
+    name: 'type-text-file-png',
+    type: 'text/plain',
+    path: '/assets/icons/png/icon-16.png',
+  },
+  {
+    name: 'type-invalid-file-jpg',
+    type: 'invalid',
+    path: '/assets/icons/jpg/icon-16.jpg',
+  },
+  {
+    name: 'type-blank-file-jpg',
+    type: '',
+    path: '/assets/icons/jpg/icon-16.jpg',
+  },
+];
