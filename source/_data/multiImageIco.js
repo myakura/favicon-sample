bd603979fb3824238b5eda670c9809d60ab0561f
Masataka Yakura <masataka.yakura@gmail.com>
2021-04-01 21:52:26 +0900

    eleventy: add support for multiple-image ico pages

---
+++ source/_data/multiImageIco.js
@@ -0,0 +1,9 @@
+const multiImageIco = [
+  {
+    title: `ICO favicon (16×16, 24×24, 32×32, 48×48, 256×256)`,
+    path: `/assets/icons/ico/icon.ico`,
+    permalink: `/format/ico/`,
+  }
+];
+
+module.exports = multiImageIco;
