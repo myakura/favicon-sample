2f524cda1b840c05d9e1f46770ef3a349747dde8
Masataka Yakura <masataka.yakura@gmail.com>
2021-04-01 21:51:33 +0900

    eleventy: add single-image ico pages to data

--- source/_data/iconsByFormat.js
+++ source/_data/iconsByFormat.js
@@ -1,18 +1,19 @@
 const sizes = [16, 24, 32, 48, 64, 96, 128, 144, 180, 192, 256, 512, 1024];
 const formats = [
-  { label: 'PNG', ext: 'png' },
-  { label: 'JPEG', ext: 'jpg' },
-  { label: 'WebP', ext: 'webp' },
-  { label: 'AVIF', ext: 'avif' },
-  { label: 'JPEG XL', ext: 'jxl' },
+  { label: 'PNG', ext: 'png', dir: 'png' },
+  { label: 'JPEG', ext: 'jpg', dir: 'jpg' },
+  { label: 'WebP', ext: 'webp', dir: 'webp' },
+  { label: 'AVIF', ext: 'avif', dir: 'avif' },
+  { label: 'JPEG XL', ext: 'jxl', dir: 'jxl' },
+  { label: 'ICO', ext: 'ico', dir: 'ico-single' },
 ];
 
-const iconsByFormat = [...formats.values()].map(({ label, ext }) => {
+const iconsByFormat = [...formats.values()].map(({ label, ext, dir }) => {
   return sizes.map((size) => {
     return {
       title: `${label} favicon (${size}×${size})`,
-      path: `/assets/icons/${ext}/icon-${size}.${ext}`,
-      permalink: `/format/${ext}/${size}/`,
+      path: `/assets/icons/${dir}/icon-${size}.${ext}`,
+      permalink: `/format/${dir}/${size}/`,
     };
   });
 }).flat();
