30b926bdcf0e5e21ee4678f949f4cdbb0f22487c
Masataka Yakura <masataka.yakura@gmail.com>
2021-04-04 20:23:11 +0900

    feat: provide more descriptive title

--- source/_data/extSpoofing.js
+++ source/_data/extSpoofing.js
@@ -1,32 +1,32 @@
 const extSpoofing = [
   {
-    name: 'png-as-txt-type-png',
+    title: 'PNG favicon served as `.txt` with type `image/png`',
     type: 'image/png',
     path: '/assets/icons/ext-spoofing/icon-16.png.txt',
     permalink: '/ext-spoofing/png-as-txt-type-png/',
   },
   {
-    name: 'png-as-txt',
+    title: 'PNG favicon served as `.txt`',
     path: '/assets/icons/ext-spoofing/icon-16.png.txt',
     permalink: '/ext-spoofing/png-as-txt/',
   },
   {
-    name: 'png-as-jpg',
+    title: 'PNG favicon served as `.jpg`',
     path: '/assets/icons/ext-spoofing/icon-16.png.jpg',
     permalink: '/ext-spoofing/png-as-jpg/',
   },
   {
-    name: 'png-as-webp',
+    title: 'PNG favicon served as `.webp`',
     path: '/assets/icons/ext-spoofing/icon-16.png.webp',
     permalink: '/ext-spoofing/png-as-webp/',
   },
   {
-    name: 'png-as-ico',
+    title: 'PNG favicon served as `.ico`',
     path: '/assets/icons/ext-spoofing/icon-16.png.ico',
     permalink: '/ext-spoofing/png-as-ico/',
   },
   {
-    name: 'ico-as-png',
+    title: 'ICO favicon served as `.png`',
     path: '/assets/icons/ext-spoofing/icon.ico.png',
     permalink: '/ext-spoofing/ico-as-png/',
   },
--- source/_data/typeMismatch.js
+++ source/_data/typeMismatch.js
@@ -1,30 +1,30 @@
 module.exports = [
   {
-    name: 'type-ico-file-png',
+    title: 'PNG favicon with type `image/vng.microsoft.icon`',
     type: 'image/vnd.microsoft.icon',
     path: '/assets/icons/png/icon-16.png',
     permalink: '/type-mismatch/type-ico-file-png/',
   },
   {
-    name: 'type-jpeg-file-png',
+    title: 'PNG favicon with type `image/jpeg`',
     type: 'image/jpeg',
     path: '/assets/icons/png/icon-16.png',
     permalink: '/type-mismatch/type-jpeg-file-png/',
   },
   {
-    name: 'type-text-file-png',
+    title: 'PNG favicon with type `text/plain`',
     type: 'text/plain',
     path: '/assets/icons/png/icon-16.png',
     permalink: '/type-mismatch/type-text-file-png/',
   },
   {
-    name: 'type-invalid-file-jpg',
+    title: 'JPEG favicon with type `invalid`',
     type: 'invalid',
     path: '/assets/icons/jpg/icon-16.jpg',
     permalink: '/type-mismatch/type-invalid-file-jpg/',
   },
   {
-    name: 'type-blank-file-jpg',
+    title: 'JPEG favicon with type ``',
     type: '',
     path: '/assets/icons/jpg/icon-16.jpg',
     permalink: '/type-mismatch/type-blank-file-jpg/',
