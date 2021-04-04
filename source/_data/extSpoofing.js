d178de44abe80caf753d41ff4dab33c958dfb74f
Masataka Yakura <masataka.yakura@gmail.com>
2021-04-04 20:16:44 +0900

    feat: move permalink to data

--- source/_data/extSpoofing.js
+++ source/_data/extSpoofing.js
@@ -3,26 +3,32 @@ const extSpoofing = [
     name: 'png-as-txt-type-png',
     type: 'image/png',
     path: '/assets/icons/ext-spoofing/icon-16.png.txt',
+    permalink: '/ext-spoofing/png-as-txt-type-png/',
   },
   {
     name: 'png-as-txt',
     path: '/assets/icons/ext-spoofing/icon-16.png.txt',
+    permalink: '/ext-spoofing/png-as-txt/',
   },
   {
     name: 'png-as-jpg',
     path: '/assets/icons/ext-spoofing/icon-16.png.jpg',
+    permalink: '/ext-spoofing/png-as-jpg/',
   },
   {
     name: 'png-as-webp',
     path: '/assets/icons/ext-spoofing/icon-16.png.webp',
+    permalink: '/ext-spoofing/png-as-webp/',
   },
   {
     name: 'png-as-ico',
     path: '/assets/icons/ext-spoofing/icon-16.png.ico',
+    permalink: '/ext-spoofing/png-as-ico/',
   },
   {
     name: 'ico-as-png',
     path: '/assets/icons/ext-spoofing/icon.ico.png',
+    permalink: '/ext-spoofing/ico-as-png/',
   },
 ];
 
--- source/_data/typeMismatch.js
+++ source/_data/typeMismatch.js
@@ -3,25 +3,30 @@ module.exports = [
     name: 'type-ico-file-png',
     type: 'image/vnd.microsoft.icon',
     path: '/assets/icons/png/icon-16.png',
+    permalink: '/type-mismatch/type-ico-file-png/',
   },
   {
     name: 'type-jpeg-file-png',
     type: 'image/jpeg',
     path: '/assets/icons/png/icon-16.png',
+    permalink: '/type-mismatch/type-jpeg-file-png/',
   },
   {
     name: 'type-text-file-png',
     type: 'text/plain',
     path: '/assets/icons/png/icon-16.png',
+    permalink: '/type-mismatch/type-text-file-png/',
   },
   {
     name: 'type-invalid-file-jpg',
     type: 'invalid',
     path: '/assets/icons/jpg/icon-16.jpg',
+    permalink: '/type-mismatch/type-invalid-file-jpg/',
   },
   {
     name: 'type-blank-file-jpg',
     type: '',
     path: '/assets/icons/jpg/icon-16.jpg',
+    permalink: '/type-mismatch/type-blank-file-jpg/',
   },
 ];
