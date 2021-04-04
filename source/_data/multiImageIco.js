2b9bf07446fa9f86172ab1610391e567bf42b258
Masataka Yakura <masataka.yakura@gmail.com>
2021-04-04 20:11:41 +0900

    chore: fix permalink style in multiImageIco

--- source/_data/multiImageIco.js
+++ source/_data/multiImageIco.js
@@ -10,37 +10,37 @@ const multiImageIco = [
   {
     title: `ICO favicon (16×16, 32×32)`,
     path: `/assets/icons/ico-multiple/icon-16-32.ico`,
-    permalink: `format/ico-multiple/16-32/`,
+    permalink: `/format/ico-multiple/16-32/`,
   },
   {
     title: `ICO favicon (16×16, 32×32, 48×48, 64×64)`,
     path: `/assets/icons/ico-multiple/icon-16-32-48-64.ico`,
-    permalink: `format/ico-multiple/16-32-48-64/`,
+    permalink: `/format/ico-multiple/16-32-48-64/`,
   },
   {
     title: `ICO favicon (16×16, 24×24, 32×32, 48×48, 256×256)`,
     path: `/assets/icons/ico-multiple/icon-16-24-32-48-256.ico`,
-    permalink: `format/ico-multiple/16-24-32-48-256/`,
+    permalink: `/format/ico-multiple/16-24-32-48-256/`,
   },
   {
     title: `ICO favicon (32×32, 16×16)`,
     path: `/assets/icons/ico-multiple/icon-32-16.ico`,
-    permalink: `format/ico-multiple/32-16/`,
+    permalink: `/format/ico-multiple/32-16/`,
   },
   {
     title: `ICO favicon (128×128, 256×256)`,
     path: `/assets/icons/ico-multiple/icon-128-256.ico`,
-    permalink: `format/ico-multiple/128-256/`,
+    permalink: `/format/ico-multiple/128-256/`,
   },
   {
     title: `ICO favicon (256×256, 512×512)`,
     path: `/assets/icons/ico-multiple/icon-256-512.ico`,
-    permalink: `format/ico-multiple/256-512/`,
+    permalink: `/format/ico-multiple/256-512/`,
   },
   {
     title: `ICO favicon (256×256, 512×512, 1024×1024)`,
     path: `/assets/icons/ico-multiple/icon-256-512-1024.ico`,
-    permalink: `format/ico-multiple/256-512-1024/`,
+    permalink: `/format/ico-multiple/256-512-1024/`,
   },
 ];
 
