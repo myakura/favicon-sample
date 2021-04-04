7b3ff4f1b96a5ce82074f8023121682d45791535
Masataka Yakura <masataka.yakura@gmail.com>
2021-04-04 21:19:38 +0900

    markup: descriptive title

--- source/_data/markup.js
+++ source/_data/markup.js
@@ -1,75 +1,83 @@
 const iconSets = [
   {
-    name: 'ico-1',
+    id: 'ico-1',
+    fileType: 'ICO',
     type: 'image/vnd.microsoft.icon',
     path: '/assets/icons/ico/icon.ico',
   },
   {
-    name: 'ico-2',
+    id: 'ico-2',
+    fileType: 'ICO',
     type: 'image/x-icon',
     path: '/assets/icons/ico/icon.ico',
   },
   {
-    name: 'png',
+    id: 'png',
+    fileType: 'PNG',
     type: 'image/png',
     path: '/assets/icons/png/icon-16.png',
   },
   {
-    name: 'jpg',
+    id: 'jpg',
+    fileType: 'JPEG',
     type: 'image/jpeg',
     path: '/assets/icons/jpg/icon-16.jpg',
   },
   {
-    name: 'webp',
+    id: 'webp',
+    fileType: 'WebP',
     type: 'image/webp',
     path: '/assets/icons/webp/icon-16.webp',
   },
   {
-    name: 'avif',
+    id: 'avif',
+    fileType: 'AVIF',
     type: 'image/avif',
     path: '/assets/icons/avif/icon-16.avif',
   },
   {
-    name: 'jxl',
+    id: 'jxl',
+    fileType: 'JPEG XL',
     type: 'image/jxl',
     path: '/assets/icons/jxl/icon-16.jxl',
   },
 ];
 
-const withShortcutWithType = iconSets.map(({ name, type, path }) => {
+
+const withShortcutWithType = iconSets.map(({ id, fileType, type, path }) => {
   return {
-    title: name,
-    permalink: `/markup/shortcut-type/${name}/`,
+    title: `${fileType} (rel="shortcut icon" type="${type}")`,
+    permalink: `/markup/shortcut-type/${id}/`,
     shortcut: true,
     type,
     path,
   };
 });
 
-const withShortcutWithoutType = iconSets.map(({ name, type, path }) => {
+const withShortcutWithoutType = iconSets.map(({ id, fileType, type, path }) => {
   return {
-    title: name,
-    permalink: `/markup/shortcut/${name}/`,
+    title: `${fileType} (rel="shortcut icon")`,
+    permalink: `/markup/shortcut/${id}/`,
     shortcut: true,
     type: null,
     path,
   };
 });
 
-const withoutShortcutWithType = iconSets.map(({ name, type, path }) => {
+const withoutShortcutWithType = iconSets.map(({ id, fileType, type, path }) => {
   return {
-    title: name,
-    permalink: `/markup/type/${name}/`,
+    title: `${fileType} (rel="icon" type="${type}")`,
+    permalink: `/markup/type/${id}/`,
     shortcut: false,
     type,
     path,
   };
 });
 
-const withoutShortcutWithoutType = iconSets.map(({ name, type, path }) => {
+const withoutShortcutWithoutType = iconSets.map(({ id, fileType, type, path }) => {
   return {
-    title: name,
-    permalink: `/markup/simple/${name}/`,
+    title: `${fileType} (rel="icon")`,
+    permalink: `/markup/simple/${id}/`,
     shortcut: false,
     type: null,
     path,
