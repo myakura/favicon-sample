a3a6693bc2de5f304516878e208a3dc01397c4af
Masataka Yakura <masataka.yakura@gmail.com>
2021-04-04 20:02:13 +0900

    feat: add extension spoofing pages

---
+++ source/_data/extSpoofing.js
@@ -0,0 +1,29 @@
+const extSpoofing = [
+  {
+    name: 'png-as-txt-type-png',
+    type: 'image/png',
+    path: '/assets/icons/ext-spoofing/icon-16.png.txt',
+  },
+  {
+    name: 'png-as-txt',
+    path: '/assets/icons/ext-spoofing/icon-16.png.txt',
+  },
+  {
+    name: 'png-as-jpg',
+    path: '/assets/icons/ext-spoofing/icon-16.png.jpg',
+  },
+  {
+    name: 'png-as-webp',
+    path: '/assets/icons/ext-spoofing/icon-16.png.webp',
+  },
+  {
+    name: 'png-as-ico',
+    path: '/assets/icons/ext-spoofing/icon-16.png.ico',
+  },
+  {
+    name: 'ico-as-png',
+    path: '/assets/icons/ext-spoofing/icon.ico.png',
+  },
+];
+
+module.exports = extSpoofing;
