7b5a478d174114b2ba45a9845f7699e9c4d43a50
Masataka Yakura <masataka.yakura@gmail.com>
2021-04-08 20:50:05 +0900

    feat: generate svg favicon pages

--- source/_data/iconsByFormat.js
+++ source/_data/iconsByFormat.js
@@ -6,6 +6,7 @@ const formats = [
   { label: 'AVIF', ext: 'avif', dir: 'avif' },
   { label: 'JPEG XL', ext: 'jxl', dir: 'jxl' },
   { label: 'ICO', ext: 'ico', dir: 'ico-single' },
+  { label: 'SVG', ext: 'svg', dir: 'svg' },
 ];
 
 const iconsByFormat = [...formats.values()].map(({ label, ext, dir }) => {
