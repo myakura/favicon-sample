1066049a2d860dd01b4cf00e2304e53ab1f0f80f
Masataka Yakura <masataka.yakura@gmail.com>
2021-06-10 01:25:38 +0900

    chore: use require directly

--- source/_data/multipleLinks.js
+++ source/_data/multipleLinks.js
@@ -1,25 +1,13 @@
-const pngPng = require('./2-icons/pngPng.js');
-const icoPng = require('./2-icons/icoPng.js');
-const svgPng = require('./2-icons/svgPng.js');
-const jpgPng = require('./2-icons/jpgPng.js');
-const webpPng = require('./2-icons/webpPng.js');
-const avifPng = require('./2-icons/avifPng.js');
-const jxlPng = require('./2-icons/jxlPng.js');
-const icoJpgPng = require('./3-icons/icoJpgPng.js');
-const icoSvgPng = require('./3-icons/icoSvgPng.js');
-const webpSvgPng = require('./3-icons/webpSvgPng.js');
-const avifJxlPng = require('./3-icons/avifJxlPng.js');
-
 module.exports = [
-  ...pngPng,
-  ...icoPng,
-  ...svgPng,
-  ...jpgPng,
-  ...webpPng,
-  ...avifPng,
-  ...jxlPng,
-  ...icoJpgPng,
-  ...icoSvgPng,
-  ...webpSvgPng,
-  ...avifJxlPng,
-];
+  ...require('./2-icons/pngPng.js'),
+  ...require('./2-icons/icoPng.js'),
+  ...require('./2-icons/svgPng.js'),
+  ...require('./2-icons/jpgPng.js'),
+  ...require('./2-icons/webpPng.js'),
+  ...require('./2-icons/avifPng.js'),
+  ...require('./2-icons/jxlPng.js'),
+  ...require('./3-icons/icoJpgPng.js'),
+  ...require('./3-icons/icoSvgPng.js'),
+  ...require('./3-icons/webpSvgPng.js'),
+  ...require('./3-icons/avifJxlPng.js'),
+]
