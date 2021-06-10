d9e045914a29b179a0b6edb719dbe10bea2fcd1f
Masataka Yakura <masataka.yakura@gmail.com>
2021-06-10 12:54:41 +0900

    feat: update multi-image ico data to the new format

--- source/_data/multiImageIco.js
+++ source/_data/multiImageIco.js
@@ -1,47 +1,61 @@
-const multiImageIco = [
-  // recommended sizes per https://docs.microsoft.com/en-us/windows/uwp/design/style/app-icons-and-logos#target-size-app-icon-assets
+module.exports = [
+  // recommended sizes
+  // https://docs.microsoft.com/en-us/windows/uwp/design/style/app-icons-and-logos#target-size-app-icon-assets
   {
     title: `ICO (16×16, 24×24, 32×32, 48×48, 256×256)`,
-    iconPath: `/assets/icons/ico/icon.ico`,
+    icons: [
+      { iconPath: `/assets/icons/ico/icon.ico` },
+    ],
     permalink: `/format/ico/`,
   },
 
-  // for checking which size to be choosen
   {
     title: `ICO (16×16, 32×32)`,
-    iconPath: `/assets/icons/ico-multiple/icon-16-32.ico`,
+    icons: [
+      { iconPath: `/assets/icons/ico-multiple/icon-16-32.ico` },
+    ],
     permalink: `/format/ico-multiple/16-32/`,
   },
   {
     title: `ICO (16×16, 32×32, 48×48, 64×64)`,
-    iconPath: `/assets/icons/ico-multiple/icon-16-32-48-64.ico`,
+    icons: [
+      { iconPath: `/assets/icons/ico-multiple/icon-16-32-48-64.ico` },
+    ],
     permalink: `/format/ico-multiple/16-32-48-64/`,
   },
   {
     title: `ICO (16×16, 24×24, 32×32, 48×48, 256×256)`,
-    iconPath: `/assets/icons/ico-multiple/icon-16-24-32-48-256.ico`,
+    icons: [
+      { iconPath: `/assets/icons/ico-multiple/icon-16-24-32-48-256.ico` },
+    ],
     permalink: `/format/ico-multiple/16-24-32-48-256/`,
   },
   {
     title: `ICO (32×32, 16×16)`,
-    iconPath: `/assets/icons/ico-multiple/icon-32-16.ico`,
+    icons: [
+      { iconPath: `/assets/icons/ico-multiple/icon-32-16.ico` },
+    ],
     permalink: `/format/ico-multiple/32-16/`,
   },
   {
     title: `ICO (128×128, 256×256)`,
-    iconPath: `/assets/icons/ico-multiple/icon-128-256.ico`,
+    icons: [
+      { iconPath: `/assets/icons/ico-multiple/icon-128-256.ico` },
+    ],
     permalink: `/format/ico-multiple/128-256/`,
   },
   {
     title: `ICO (256×256, 512×512)`,
-    iconPath: `/assets/icons/ico-multiple/icon-256-512.ico`,
+    icons: [
+      { iconPath: `/assets/icons/ico-multiple/icon-256-512.ico` },
+    ],
     permalink: `/format/ico-multiple/256-512/`,
   },
   {
     title: `ICO (256×256, 512×512, 1024×1024)`,
-    iconPath: `/assets/icons/ico-multiple/icon-256-512-1024.ico`,
+    icons: [
+      { iconPath: `/assets/icons/ico-multiple/icon-256-512-1024.ico` },
+    ],
     permalink: `/format/ico-multiple/256-512-1024/`,
   },
 ];
-
-module.exports = multiImageIco;
--- source/_data/pages.js
+++ source/_data/pages.js
@@ -1,9 +1,7 @@
-const multiImageIco = require('./multiImageIco.js');
 const typeMismatch = require('./typeMismatch.js');
 const extSpoofing = require('./extSpoofing.js');
 
 module.exports = [
-  ...multiImageIco,
   ...typeMismatch,
   ...extSpoofing,
 ]
