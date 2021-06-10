33177acc0b3d67d9187b53e4d832dcd85430f8b2
Masataka Yakura <masataka.yakura@gmail.com>
2021-06-10 12:54:08 +0900

    chore: remove legacy data in favor of new ones

--- source/_data/pages.js
+++ source/_data/pages.js
@@ -1,12 +1,8 @@
-const iconsByFormat = require('./iconsByFormat.js');
-const singleImageIco = require('./singleImageIco.js');
 const multiImageIco = require('./multiImageIco.js');
 const typeMismatch = require('./typeMismatch.js');
 const extSpoofing = require('./extSpoofing.js');
 
 module.exports = [
-  ...iconsByFormat,
-  ...singleImageIco,
   ...multiImageIco,
   ...typeMismatch,
   ...extSpoofing,
