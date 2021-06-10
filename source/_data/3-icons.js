adc3ef17dcefa4ccc4fdfe9f10497151c4d8ac1d
Masataka Yakura <masataka.yakura@gmail.com>
2021-06-10 12:05:10 +0900

    feat: add separate data for 2 and 3 icon pages

---
+++ source/_data/3-icons.js
@@ -0,0 +1,6 @@
+module.exports = [
+  ...require('./3-icons/icoJpgPng.js'),
+  ...require('./3-icons/icoSvgPng.js'),
+  ...require('./3-icons/webpSvgPng.js'),
+  ...require('./3-icons/avifJxlPng.js'),
+];
