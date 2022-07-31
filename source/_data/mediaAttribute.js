f19947288d66ecf106cbf7c6c213c1dd1e85be92
Masataka Yakura <masataka.yakura@gmail.com>
2022-08-01 00:23:41 +0900

    feat: add media+nomedia pages

--- source/_data/mediaAttribute.js
+++ source/_data/mediaAttribute.js
@@ -29,4 +29,70 @@ module.exports = [
     ],
     permalink: '/media/print/',
   },
+  {
+    title: 'PNG (media="all"), PNG',
+    icons: [
+      {
+        href: '/assets/icons/png/icon-16.png',
+        media: 'all',
+      },
+      { href: '/assets/icons/png-dark/icon-16.png' },
+    ],
+    permalink: '/media/all-nomedia/',
+  },
+  {
+    title: 'PNG (media="screen"), PNG',
+    icons: [
+      {
+        href: '/assets/icons/png/icon-16.png',
+        media: 'screen',
+      },
+      { href: '/assets/icons/png-dark/icon-16.png' },
+    ],
+    permalink: '/media/screen-nomedia/',
+  },
+  {
+    title: 'PNG (media="print"), PNG',
+    icons: [
+      {
+        href: '/assets/icons/png/icon-16.png',
+        media: 'print',
+      },
+      { href: '/assets/icons/png-dark/icon-16.png' },
+    ],
+    permalink: '/media/print-nomedia/',
+  },
+  {
+    title: 'PNG, PNG (media="all")',
+    icons: [
+      { href: '/assets/icons/png-dark/icon-16.png' },
+      {
+        href: '/assets/icons/png/icon-16.png',
+        media: 'all',
+      },
+    ],
+    permalink: '/media/nomedia-all/',
+  },
+  {
+    title: 'PNG, PNG (media="screen")',
+    icons: [
+      { href: '/assets/icons/png-dark/icon-16.png' },
+      {
+        href: '/assets/icons/png/icon-16.png',
+        media: 'screen',
+      },
+    ],
+    permalink: '/media/nomedia-screen/',
+  },
+  {
+    title: 'PNG, PNG (media="print")',
+    icons: [
+      { href: '/assets/icons/png-dark/icon-16.png' },
+      {
+        href: '/assets/icons/png/icon-16.png',
+        media: 'print',
+      },
+    ],
+    permalink: '/media/nomedia-print/',
+  },
 ];
