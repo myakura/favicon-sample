fa00fea2e2f0d75cede58b7da974fa2bf667a416
Masataka Yakura <masataka.yakura@gmail.com>
2022-08-27 10:46:53 +0900

    feat: add more PNG+ICO+SVG pages

--- source/_data/mixed/pngIcoSvgSizesType.js
+++ source/_data/mixed/pngIcoSvgSizesType.js
@@ -12,6 +12,32 @@ const pngSizesIcoAnySvgType = sizes.map((size) => {
   };
 });
 
+const pngSizesIcoAnySvg = sizes.map((size) => {
+  return {
+    title: `PNG (${size}x${size}), ICO (any), SVG`,
+    icons: [
+      { href: `/assets/icons/png/icon-${size}.png`, sizes: `${size}x${size}` },
+      { href: `/assets/icons/ico/icon.ico`, sizes: 'any' },
+      { href: '/assets/icons/svg/icon-16.svg' },
+    ],
+    permalink: `/mixed/png-ico-svg-sizes-type/png-${size}-ico-any-svg/`,
+  };
+});
+
+const pngSizesIcoSvg = sizes.map((size) => {
+  return {
+    title: `PNG (${size}x${size}), ICO, SVG`,
+    icons: [
+      { href: `/assets/icons/png/icon-${size}.png`, sizes: `${size}x${size}` },
+      { href: `/assets/icons/ico/icon.ico` },
+      { href: '/assets/icons/svg/icon-16.svg' },
+    ],
+    permalink: `/mixed/png-ico-svg-sizes-type/png-${size}-ico-svg/`,
+  };
+});
+
 module.exports = [
   ...pngSizesIcoAnySvgType,
+  ...pngSizesIcoAnySvg,
+  ...pngSizesIcoSvg,
 ];
